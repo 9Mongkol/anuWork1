02352375be9293d866cc6ddedb7915173362d28c
9Mongkol <9mongkol2005@gmail.com>
2024-06-21 11:29:36 +0700

    commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export default function Contact() {
+  return (
+    <>
+    <h1><center>Home Page</center></h1>
+    <br></br>
+    <center>
+    <Link href="/">Home</Link> |
+    <Link href="/about">About</Link> |
+    <Link href="/Service">Service</Link> | 
+    <Link href="/Contact">Contact</Link> |
+    </center>
+    </>
+  );
+}
